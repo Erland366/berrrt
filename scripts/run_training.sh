939abcdb9e74837e57debde8021df1755f1508a6
Erland366 <erland.pg366@gmail.com>
2024-04-18 01:10:47 +0700

    New script for auto turnoff vastai

--- scripts/run_training.sh
+++ scripts/run_training.sh
@@ -1,5 +1,7 @@
 bash scripts/print_header.sh
 
+source .env
+
 python main.py mode=full modules=bert modules_name=bert
 
 # python main.py mode=sanity_check modules.layer_start=0 modules.layer_end=11 modules.gate=sigmoid modules=berrrt_gate modules_name=berrrt_gate
@@ -69,3 +71,15 @@ for ((layer=0; layer<=10; layer++)); do
   # Run main.py with the current layer
   python main.py mode=full modules.layer_start=$layer modules.layer_end=11 modules=berrrt_gate modules_name=berrrt_gate modules.gate=sigmoid
 done
+
+
+if [ "$#" -eq 1 ]; then
+    ID=$1
+    API_KEY=$VASTAI_API_KEY
+
+    curl --location -g --request DELETE "https://console.vast.ai/api/v0/instances/${ID}/" \
+         --header "Accept: application/json" \
+         --header "Authorization: Bearer ${API_KEY}"
+else
+    echo "ID not provided. Skipping delete vastai machine."
+fi
